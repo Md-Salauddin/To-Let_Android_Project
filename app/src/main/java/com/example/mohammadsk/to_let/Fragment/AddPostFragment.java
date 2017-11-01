cdde860d90aa783d85ad59aac23133a4a321b947
MohammadSk <salauddin.csese@gmail.com>
2017-11-01 14:52:39 +0600

    create some layout

--- app/src/main/java/com/example/mohammadsk/to_let/Fragment/AddPostFragment.java
+++ app/src/main/java/com/example/mohammadsk/to_let/Fragment/AddPostFragment.java
@@ -3,6 +3,7 @@ package com.example.mohammadsk.to_let.Fragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/com/example/mohammadsk/to_let/Main3Activity.java
+++ app/src/main/java/com/example/mohammadsk/to_let/Main3Activity.java
@@ -5,6 +5,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.mohammadsk.to_let.Fragment.AddPostFragment;
 import com.example.mohammadsk.to_let.Fragment.UploadImageFragment;
